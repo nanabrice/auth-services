b46917f42947678246c37d751cd3e6b8599f1f23
nanabrice <byce.0002@gmail.com>
2023-06-01 09:36:37 +0100

    y

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,25 +1,25 @@
-import Navbar from '../components/Navigationbar';
+import Navbar from "../components/Navigationbar";
 
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css'
+import Container from "react-bootstrap/esm/Container";
 
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
 
 const LandingPage = () => {
-    return (
-        <>
-            <Navbar />
-            <h2>This is the landing page</h2>
-            <div>
-                <Link to="/Register">Sign up</Link>
-            </div>
-            <div>
-                <Link to="/Login">Login</Link>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+
+      <Container>
+        <h2>This is the landing page</h2>
+        <div>
+          <Link to="/Register">Sign up</Link>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default LandingPage;
