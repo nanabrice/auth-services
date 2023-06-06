68bcc883c6033ceaee51a443c26894678d7b6fa3
nanabrice <byce.0002@gmail.com>
2023-06-06 03:59:41 +0100

    y

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,54 @@
-import {Link} from 'react-router-dom';
+import AppNavBar from "../components/AppNavBar";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css'
+import Stack from "react-bootstrap/esm/Stack";
+import Container from "react-bootstrap/esm/Container";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShareNodes, faTable } from "@fortawesome/free-solid-svg-icons";
 
+import { Link } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
 
 const Home = () => {
-    return (
-        <>
-            <h2>Home page of app</h2>
-        </>
-    );
+  return (
+    <>
+      <AppNavBar />
+
+      <Container className="full-body">
+        <div className="m-5">
+          <Stack direction="horizontal" gap={3}>
+            <Stack gap={1}>
+              <div className="">
+                <h2>EVIL Corp.</h2>
+              </div>
+              <div className="">
+                <p>Public | x member | Add Bookmark</p>
+              </div>
+            </Stack>
+            <div className="">
+              <p>
+                <FontAwesomeIcon
+                  icon={faShareNodes}
+                  style={{ color: "#00000" }}
+                  size="lg"
+                  className="me-2"
+                />
+                Share
+              </p>
+            </div>
+          </Stack>
+        </div>
+
+        <Stack direction="horizontal" gap={2}>
+        <div>
+            <FontAwesomeIcon icon={faTable} />
+        </div>
+        </Stack>
+      </Container>
+    </>
+  );
 };
 
 export default Home;
